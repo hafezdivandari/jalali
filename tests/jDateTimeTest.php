9a885581986ddf156fd16cca9b863f24cd0f9419
hafezdivandari <hafezdivandari@gmail.com>
2016-06-20 10:58:11 +0430

    Timezone test added
    Timezone issue and it's solution (using `new DateTime($value, new \DateTimeZone("UTC"))->getTimestamp()` instead of `strtotime($value)`)

--- tests/jDateTimeTest.php
+++ tests/jDateTimeTest.php
@@ -1,7 +1,6 @@
 <?php
 
 use Morilog\Jalali\jDateTime;
-use Morilog\Jalali\jDate;
 
 class jDateTimeTest extends PHPUnit_Framework_TestCase
 {
@@ -36,6 +35,12 @@ class jDateTimeTest extends PHPUnit_Framework_TestCase
         $this->assertTrue(jDateTime::strftime('Y-m-d', strtotime('2016-05-8')) === '1395-02-19');
         $this->assertTrue(jDateTime::convertNumbers(jDateTime::strftime('Y-m-d', strtotime('2016-05-8'))) === '۱۳۹۵-۰۲-۱۹');
         $this->assertFalse(jDateTime::strftime('Y-m-d', strtotime('2016-05-8')) === '۱۳۹۵-۰۲-۱۹');
+        
+        $this->assertTrue(date_default_timezone_set('Asia/Tehran'));
+        $this->assertTrue(jDateTime::strftime('Y-m-d', strtotime('2016-05-8')) === '1395-02-19');
+        $this->assertTrue(jDateTime::strftime('Y-m-d', (new \DateTime('2016-05-8', new \DateTimeZone("UTC")))->getTimestamp()) === '1395-02-19');
+        
+        $this->assertTrue(date_default_timezone_set('UTC'));
     }
 
     public function test_parseFromPersian()
@@ -46,11 +51,6 @@ class jDateTimeTest extends PHPUnit_Framework_TestCase
         $this->assertEquals(1393, $date['year']);
         $this->assertEquals(03, $date['month']);
         $this->assertEquals(27, $date['day']);
-
-        $date = jDateTime::parseFromFormat('Y-m-d H:i:s', '1395-03-15 21:00:00');
-        $this->assertEquals(21, $date['hour']);
-        $this->assertEquals(0, $date['minute']);
-        $this->assertEquals(0, $date['second']);
     }
 
     public function testCreateDateTimeFormFormat()
@@ -71,11 +71,5 @@ class jDateTimeTest extends PHPUnit_Framework_TestCase
         $this->assertTrue($carbon instanceof \Carbon\Carbon);
         $this->assertTrue($carbon->day === 14);
         $this->assertTrue('2016-02-14 15:00:00' === $carbon->format('Y-m-d H:i:s'));
-
-        $jalaiDateFormatted = jDate::forge($carbon->toDateString())->format('Y-m-d H:i:s');
-        $jalaiDateTimeFormatted = jDate::forge($carbon->toDateTimeString())->format('Y-m-d H:i:s');
-        $this->assertFalse( $jalaiDateFormatted === '1394-11-25 15:00:00');
-        $this->assertTrue( $jalaiDateTimeFormatted === '1394-11-25 15:00:00');
-
     }
 }
